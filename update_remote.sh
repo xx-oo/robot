b97134e53a7f3768c8a42de2b1f2c66e3f442593
xx-oo <57185583+xx-oo@users.noreply.github.com>
2019-10-30 23:21:45 +0800

    3rdparty

---
+++ update_remote.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+echo "Update Robot Code>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+#expect shell/update_remote_robot.expect
+#expect shell/update_remote_raspi.expect
+expect shell/update_remote_squared.expect
+echo "==============================Done=======================================>"
